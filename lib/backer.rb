1757235b0aef99eff7e1f33dd3aeca74d6c38b4c
Taylor Booth <taylorkbooth@gmail.com>
2020-09-26 02:41:13 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,15 @@
 class Backer
   
     attr_reader :name
-    attr_accessor :backed_project
+    attr_accessor :backed_projects
 
   def initialize(name)
     @name = name
-    @backed_project = []
+    @backed_projects = []
   end
   
-  def backed_project(project)
-    @backed_project << project
+  def backed_projects(project)
+    @backed_projects << project
     project.backers << self
   end
  
